60235b30a6bd9cbb76ebb29080c5108d72727c1d
fhirfly <paycbdforward@gmail.com>
2021-04-30 11:42:14 -0400

    working to make patient service production ready

--- gcp/metadata_endpoints.js
+++ gcp/metadata_endpoints.js
@@ -1,14 +1,7 @@
 'use strict';
 const metadataLib = require('../lib/metadata_endpoints');
 
-//Metadata endpoints - GCP Lambda Interface
-//See the metadata library for more detail.
 exports.smartConfigHandler = async (req, res) => {
-	var smartConfigResult = await metadataLib.smartConfigHandler()
+	var smartConfigResult = await metadataLib.smartConfigHandler(req.query)
 	res.send( smartConfigResult);
-}
-
-exports.metadataHandler = async (req, res) => {
-	var metadataResult = await metadataLib.metadataHandler()
-	res.send( metadataResult);
 }
