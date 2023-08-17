7a3fa424bf448435a40badeda784ab41ca0bf92a
fhirfly <richard.braman@fhirfly.io>
2023-08-17 03:27:06 -0400

    added error handling

--- gcp/patient_picker.js
+++ gcp/patient_picker.js
@@ -3,23 +3,33 @@ const patientPickerLib = require('../lib/patient_picker')
 
 
 exports.patientPickerGetHandler = async (req, res) => {
-	var getResult = await patientPickerLib.getHandler(req.headers[Object.keys(req.headers).find(key => key.toLowerCase() === 'cookie')])
-	res.status = getResult.statusCode;
-	res.setHeader('Content-Type', 'text/html');
-	res.send(getResult.body);
+	try{
+		var getResult = await patientPickerLib.getHandler(req.headers[Object.keys(req.headers).find(key => key.toLowerCase() === 'cookie')])
+		res.status = getResult.statusCode;
+		res.setHeader('Content-Type', 'text/html');
+		res.send(getResult.body);
+	}
+	catch(e){
+		res.send(400);
+	}
 }
 
 exports.patientPickerPostHandler = async (req, res) => {
-	var postResult = await patientPickerLib.postHandler(JSON.stringify(req.body), req.headers[Object.keys(req.headers).find(key => key.toLowerCase() === 'cookie')])
-	res.status(postResult.statusCode);
-	res.setHeader('Content-Type', 'application/json');
-	res.setHeader('Location', postResult.location);
-	res.setHeader('Set-Cookie', [
-		'appProxyAuthzState=' + postResult.appProxyAuthzStateCookie + '; Secure; HttpOnly',
-		'apiAccessToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT',
-		'pickerAuthzState=; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-	])
-	res.send(postResult.body);
+	try{
+		var postResult = await patientPickerLib.postHandler(JSON.stringify(req.body), req.headers[Object.keys(req.headers).find(key => key.toLowerCase() === 'cookie')])
+		res.status(postResult.statusCode);
+		res.setHeader('Content-Type', 'application/json');
+		res.setHeader('Location', postResult.location);
+		res.setHeader('Set-Cookie', [
+			'appProxyAuthzState=' + postResult.appProxyAuthzStateCookie + '; Secure; HttpOnly',
+			'apiAccessToken=; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+			'pickerAuthzState=; expires=Thu, 01 Jan 1970 00:00:00 GMT'
+		])
+		res.send(postResult.body);
+	}
+	catch(e){
+		res.send(400)
+	}
 }
 
 
